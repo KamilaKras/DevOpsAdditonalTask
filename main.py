108e17850776e5a250f674218e343b98ec967235
Kajot-dev <jjaruszewski@outlook.com>
2024-12-04 10:33:09 +0100

    Added world size

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def hello_world():
-    print("Hello you")
-    print("Have a nice day ! :)")
+def hello_world(size: int = 3):
+    o_s = "o" * size
+    i_s = "i" * size
+    print(f"Hello y{o_s}u")
+    print(f"Have a n{i_s}ce day ! :)")
 
 if __name__ == "__main__":
     hello_world()
